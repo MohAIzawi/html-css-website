d9ed3c7c59e39c0f00158c008ab160125cdbc9ef
Mohamed Al Azawi <alazawimohamed@gmail.com>
2024-05-27 09:21:09 +0200

    update

---
+++ Src/JS/GuessNumber.js
@@ -0,0 +1,68 @@
+        <table class="table table-bordered table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th>Column 1</th>
+              <th>Column 2</th>
+              <th>Column 3</th>
+              <th>Column 4</th>
+              <th>Column 5</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+            </tr>
+            <tr>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+            </tr>
+            <tr>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+            </tr>
+            <tr>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+            </tr>
+            <tr>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+            </tr>
+            <tr>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+            </tr>
+            <tr>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+            </tr>
+            <tr>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+              <td>salut</td>
+            </tr>
+          </tbody>
